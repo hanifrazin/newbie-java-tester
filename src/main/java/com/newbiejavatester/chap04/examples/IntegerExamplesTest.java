1cc1ad38fa80fe6e2c92fecff92be8628728d713
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2025-03-04 14:47:11 +0700

    chapter 4 tentang Integer

--- src/main/java/com/newbiejavatester/chap04/examples/IntegerExamplesTest.java
+++ src/main/java/com/newbiejavatester/chap04/examples/IntegerExamplesTest.java
@@ -3,6 +3,7 @@ package com.newbiejavatester.chap04.examples;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class IntegerExamplesTest {
 
@@ -25,5 +26,23 @@ public class IntegerExamplesTest {
 
         String twenty1 = Integer.toHexString(21);
         assertEquals("21 becomes 15","15",twenty1);
+    }
+
+    @Test
+    public void confirmMaxMinIntegerSize(){
+        String MinValue = String.valueOf(Integer.MIN_VALUE);
+        assertEquals("MIN_VALUE equals -2147483648","-2147483648",MinValue);
+
+        String MaxValue = String.valueOf(Integer.MAX_VALUE);
+        assertEquals("MAX_VALUE equals 2147483647","2147483647",MaxValue);
+    }
+
+    @Test
+    public void differentIntegerAndInt(){
+        Integer firstFour = 4;
+        Integer secondFour = 4;
+        assertEquals("Assertion 1 : ",firstFour,secondFour);
+        assertTrue("Assertion compare 2 : ",firstFour == secondFour);
+        assertTrue("Assertion compare 3 : ",firstFour.equals(secondFour));
     }
 }
